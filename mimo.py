01ae0562e6def6130de07bac9c8d53599cd19892
oakca <okanakca@gmail.com>
2019-01-17 14:46:20 +0100

    Memory used info is added

--- mimo.py
+++ mimo.py
@@ -106,9 +106,30 @@ def comparison(u_model, o_model):
     """
     # check objective difference
     if u_model.obj() != o_model.objective():
+        print('OBJECTIVE')
         print('urbs\t', u_model.obj())
         print('oemof\t', o_model.objective())
         print('Diff\t', u_model.obj() - o_model.objective())
+
+    print('----------------------------------------------------')
+
+    # memory info
+    with open('urbs_log.txt', 'r') as urbslog:
+        mem_urbs = urbslog.read().replace('\n', ' ')
+        mem_urbs = float(mem_urbs[mem_urbs.find('Memory used:')+12:
+								  mem_urbs.find('Mb')])
+
+    with open('oemof_log.txt', 'r') as oemoflog:
+        mem_oemof = oemoflog.read().replace('\n', ' ')
+        mem_oemof = float(mem_oemof[mem_oemof.find('Memory used:')+12:
+								    mem_oemof.find('Mb')])
+
+    # check memory difference
+    if mem_urbs != mem_oemof:
+        print('Memory Used')
+        print('urbs\t', mem_urbs, ' Mb')
+        print('oemof\t', mem_oemof, ' Mb')
+        print('Diff\t', mem_urbs - mem_oemof, ' Mb')
 
     # create oemof energysytem
     o_model = solph.EnergySystem()
@@ -141,7 +162,7 @@ def create_um(input_data, timesteps):
 
     # solve model and read results
     optim = SolverFactory('glpk')
-    result = optim.solve(model, tee=False)
+    result = optim.solve(model, logfile='urbs_log.txt', tee=False)
 
     # write LP file
     filename = os.path.join(os.path.dirname(__file__), 'mimo_urbs.lp')
@@ -170,7 +191,8 @@ def create_om(input_data, timesteps):
     es, model = oemofm.create_model(input_data, timesteps)
 
     # solve model and read results
-    model.solve(solver='glpk', solve_kwargs={'tee': False})
+    model.solve(solver='glpk', solve_kwargs={'logfile':'oemof_log.txt',
+											 'tee': False})
 
     # write LP file
     filename = os.path.join(os.path.dirname(__file__), 'mimo_oemof.lp')
